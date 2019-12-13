a5996ff8fe8db89176aa2ae2f86466d073a2984d
yongjie <yongyongjiejie1995@hotmail.com>
2019-12-14 01:02:52 +0800

    Added add job

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'jobposts#index', as: 'home'
-  get 'addJob' => 'jobposts#addJob', as: 'addJob'
-  get 'viewJob' => 'jobposts#viewJob', as: 'viewJob'
+  root 'home#index', as: 'home'
+  get 'new' => 'jobposts#new', as: 'new'
+  get 'viewJob' => 'jobposts#index', as: 'viewJob'
   resources :jobposts
 end
