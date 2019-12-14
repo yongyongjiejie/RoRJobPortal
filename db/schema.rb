aae8fe3c0b7fba746121ec14d7584b8e1e690042
yongjie <yongyongjiejie1995@hotmail.com>
2019-12-14 19:02:16 +0800

    Added signup and login

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_144715) do
+ActiveRecord::Schema.define(version: 2019_12_14_082051) do
 
   create_table "jobposts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
